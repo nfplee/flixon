c85f552d2b7163dd974bda6c855f616d31ff2f8b
Lee Timmins <lee.timmins@outlook.com>
2023-02-22 16:39:56 +0000

    Add conditions to Route attribute

--- src/Flixon/Routing/Annotations/Route.php
+++ src/Flixon/Routing/Annotations/Route.php
@@ -5,5 +5,5 @@ namespace Flixon\Routing\Annotations;
 use Attribute;
 use Symfony\Component\Routing\Annotation\Route as RouteBase;
 
-#[Attribute]
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 class Route extends RouteBase { }
